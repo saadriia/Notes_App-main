7ebd1f45007e02dfd635c3101763bbf96310e32d
saadriia <[saadriasr005@gmail.com]>
2024-05-22 23:57:38 +0300

    creatnoteCubit

--- lib/view/Nots_view.dart
+++ lib/view/Nots_view.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/cubit/create_note_cubit_cubit.dart';
-//import 'package:notes_app/view/widgets/Nots_view_body.dart';
+ //import 'package:notes_app/view/widgets/Nots_view_body.dart';
 
 import 'widgets/Add_note_ButtonSheet.dart';
 import 'widgets/Nots_view_body.dart';
